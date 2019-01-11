626fef2d154249434a34985ae6cc6beed35d26bd
mrfrac <fracturer@gmail.com>
2019-01-11 18:03:35 +0500

    Second commit

--- server.js
+++ server.js
@@ -1,16 +1,29 @@
 const express = require('express');
-const bodyParser = require('body-parser')
 const path = require('path');
+const knex = require('knex')({
+  client: "sqlite3",
+    useNullAsDefault: true,
+    connection: {
+      filename: "./db.sqlite",
+      debug: true
+    }
+});
+
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/ping', function (req, res) {
- return res.send('pong');
+app.get('/ping', (req, res) => {
+  return res.send('pong');
 });
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.get('/api/event_types', async (req, res) => {
+  const types = await knex("event_types").select();
+  res.send(types).status(200);
 });
 
 app.listen(process.env.PORT || 8080);
